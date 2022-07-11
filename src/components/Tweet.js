2931cf3ce8da72d037a724b8891ec1a8d480c0d5
Nico M <nico_16bcn@hotmail.com>
2022-07-11 18:26:24 +0200

    iteración 6

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,16 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+        <User userData={props.tweet.user} />
+        <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <p className="message">
